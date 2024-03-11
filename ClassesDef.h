0d41d84f6ed38c31eefadbcbe394f831f95fe5e9
CHAINRICH-NIKOLAOS MARKANTONIS <csd5449@csd.uoc.gr>
2024-03-11 19:50:29 +0200

    finished question 1

--- ClassesDef.h
+++ ClassesDef.h
@@ -22,8 +22,67 @@ public:
     float posX() const;
     float posY() const;
     float posZ() const;
+
+    float Radius() const;
 private:
     vec3 possition;
     vec3 color;
     float radius;
 };
+
+/*-------------------------------------------------------------------------------------------------------------------------------
+ * functions*/
+
+/* vec3 crossProduct: calulates the cross product (εξωτερικό γινόμενο) of 
+ two vectors and return the final correct one  */
+vec3 crossProduct(const vec3 &A, const vec3 &B) {
+    return vec3(A.y() * B.z() - A.z() * B.y(), -A.x() * B.z() + A.z() * B.x(), A.x() * B.y() - A.y() * B.x());
+}
+
+/* bool Dist: calculates whether a ray hits a sphere, by finding
+ the disnace between the two and if it's less (or equal) it return true.
+ 
+ * Inspiration on how to calculate said distance from HY-111 */
+float Dist(const ray &Ray, const Sphere &sphere) {
+    /* @param PS: vector that starts from one random point of the ray (the one saved)
+     and ends in the center of the Sphere  
+     * @param PS_times_v: cross product between PS and the coords of the vector*/
+    vec3 PS(sphere.posX() - Ray.point.x(), sphere.posY() - Ray.point.y(), sphere.posZ() - Ray.point.z());
+    vec3 PS_times_v = crossProduct(PS, Ray.coords);
+
+    return (float) (PS_times_v.length() / Ray.coords.length());
+}
+
+// Spheres
+Sphere::Sphere(vec3 _possition, vec3 _color, float _radius) {
+    possition = _possition;
+    color = _color;
+    radius = _radius;
+}
+
+bool Sphere::Hit(const ray &Ray, double &dist) const {
+    // making sure the ray is even going somewhat the right dirrection (so that we dont show a cyrcle 
+    // even if it is exactly behinf the camera)
+    if (this->posX() - Ray.coords.x() < 0 || this->posY() - Ray.coords.y() < 0 || this->posZ() - Ray.coords.z() < 0) return 0;
+
+    float distance = Dist(Ray, *this);
+
+    if (distance <= dist && distance < Radius()) return 1;
+    return 0;
+}
+
+float Sphere::posX() const {
+    return (float) possition.x();
+}
+
+float Sphere::posY() const {
+    return (float) possition.y();
+}
+
+float Sphere::posZ() const {
+    return (float) possition.z();
+}
+
+float Sphere::Radius() const {
+    return (float) radius;
+}
